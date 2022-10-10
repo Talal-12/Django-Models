a0b2c45b30d8c9c18f2555543657ce948000ba95
Talal <talal.alothman@gmail.com>
2022-10-10 23:34:20 +0300

    done

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
