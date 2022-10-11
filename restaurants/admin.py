f8267cb5063bb66f6d413298c07f5d676e740d71
Talal <talal.alothman@gmail.com>
2022-10-11 19:01:25 +0300

    done

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Restaurant
 
 # Register your models here.
+
+# TO CUSTOMIZE ADMIN FIELDS:
+@admin.site.register(Restaurant)
+class RestaurantAdmin(admin.Model(Admin)):
+    fields = ("name", "opening_time")
+
+# TO RUN THE ADMIN FIELD REGULARLY (WITHOUT MANIPULATION):
+# admin.site.register(Restaurant)
--- restaurants/models.py
+++ restaurants/models.py
@@ -5,8 +5,14 @@ from django.db import models
 
 
 class Restaurant(models.Model):
+    class RestaurantType(models.TextChoices):
+        vegan = "vegan"
+        Vegetarian = "Vegetarian"
+        Keto = "keto"
+
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    type = models.CharField(max_length=30, choices=RestaurantType.choices)
+    description = models.TextField(DEFAULT="")
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     created_at = models.DateTimeField(auto_now_add=True)
